f64d561ab7d02c932a6808260e3b9b16d85781f4
Kosaaaaa <oskarkosobucki@gmail.com>
2020-04-03 16:28:37 +0200

    clean

--- script.js
+++
@@ -1,6 +0,0 @@
-// function showClass(){
-//     let imgs = [document.getElementById('pro1'), document.getElementById('pro2'), document.getElementById('pro3')];
-//     for (let i=0; i<3;i++){
-//         imgs[i].classList.add('showMe')
-//     }
-// }
